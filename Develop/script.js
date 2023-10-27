a3124f3f0a04d21ca185c44123b656f69c917f4f
Samuel <salvadomo@hotmail.com>
2023-10-27 14:14:23 -0600

    prompt for pw length and length check

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Function to generate a random password
 function generatePassword() {
+  // Prompt the user for the password length between 8 and 128 characters
+  var passwordLength = prompt("Enter the password length (between 8 and 128 characters):");
 
+  // Check if the entered length is within the specified range
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    return '';
+  }
+
+  return password;
 }
 
 // Write password to the #password input
