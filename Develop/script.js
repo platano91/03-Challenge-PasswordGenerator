39c2b8bda49ea257c8edf0ef18194d0e08574867
Samuel <salvadomo@hotmail.com>
2023-10-27 14:19:54 -0600

    added characters to function

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,17 @@ function generatePassword() {
   // Prompt the user for the password length between 8 and 128 characters
   var passwordLength = prompt("Enter the password length (between 8 and 128 characters):");
 
-  // Check if the entered length is within the specified range
+  // Check if the entered length is within range
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert("Please enter a valid password length between 8 and 128 characters.");
     return '';
   }
+
+  // Define the character sets for password
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var numberChars = "0123456789";
+  var specialChars = "!@#$%^&*()_-+=<>?";
 
   return password;
 }
