d926384bafcfeb5ca7a512beb270b0c1b6bb3f2f
Samuel <salvadomo@hotmail.com>
2023-10-27 14:25:46 -0600

    prompts for character set selections

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Function to generate a random password
+// Function to generate random password
 function generatePassword() {
-  // Prompt the user for the password length between 8 and 128 characters
+  // Prompt user for password length between 8 and 128 characters
   var passwordLength = prompt("Enter the password length (between 8 and 128 characters):");
 
-  // Check if the entered length is within range
+  // Check if entered length is within range
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert("Please enter a valid password length between 8 and 128 characters.");
     return '';
   }
 
-  // Define the character sets for password
+  // Define character sets for password
   var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
   var numberChars = "0123456789";
   var specialChars = "!@#$%^&*()_-+=<>?";
+
+  // Prompt user for character set selection
+  var useUppercase = confirm("Include uppercase characters?");
+  var useLowercase = confirm("Include lowercase characters?");
+  var useNumbers = confirm("Include numbers?");
+  var useSpecial = confirm("Include special characters?");
 
   return password;
 }
